a030d0264623960ba0a0ab2f83e8ca45294f6da9
Jhon Alexander Restrepo Henao <112440657+JhonRestrepo26@users.noreply.github.com>
2022-11-28 03:13:47 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -16,6 +16,35 @@ def ingest_data():
 
     #
     # Inserte su código aquí
-    #
+    
+    #Lectura
+    with open('/tmp/clusters_report.txt','r') as files:
+        datico=[line for line in files]
+    body=datico[4:] #Eliminando filas 'innecesarias'
+    
+    body='\n'.join(body).split('\n\n\n\n')  #Patrón identificado, clusters como registros (elementos de lista)
+    
+    dic={'cluster':[],'cantidad_de_palabras_claves':[], 'porcentaje_de_palabras_claves':[],'principales_palabras_clave':[]} #Simplificación nombres de col
 
+    #'Hack':
+    
+    for i in body:
+      clus=''
+      qua=''
+      perc=''
+      for j in range(4):
+        if i[j+3] != ' ':
+          clus+=i[j+3]
+        if i[j+9] != ' ':
+          qua+=i[j+9]
+        if i[j+25] != ' ':
+          perc+=i[j+25]  
+        if j==3:
+          dic['cluster'].append(int(clus))
+          dic['cantidad_de_palabras_claves'].append(int(qua))
+          dic['porcentaje_de_palabras_claves'].append(float(perc.replace(',','.')))
+          dic['principales_palabras_clave'].append(i[40:])
+    
+    df=pd.DataFrame(dic)    #'Hack' #2
+    
     return df
